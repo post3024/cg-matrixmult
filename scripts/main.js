1b626f45d417e6a77f641e1ad85447759ffc0188
post3024 <43583062+post3024@users.noreply.github.com>
2020-03-11 11:15:36 -0500

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -12,14 +12,31 @@ function CalculateCompoundTransform(transforms) {
 
     compound_transform = new Matrix(4, 4); // change / remove this
 
+    if(transforms.length == 1) {
+        compound_transform = transforms[0].mat4x4;
+    }
+    else {
+        var matrices = [];
+        for(var i = transforms.length - 1; i >= 0; i--) {
+            matrices.push(transforms[i].mat4x4);
+        }
+        compound_transform = Matrix.multiply(matrices);
+    }
+
     return compound_transform;
 }
 
 // automatically called whenever compound transform changes
 function CalculateTransformedVertex(vertex) {
+
+    console.log(vertex);
     // multiple vertex by compound_transform
     // `final_vertex = Matrix.multiply(...)`
     var final_vertex = new Vector(4); // change / remove this
+
+    var matrices = [compound_transform, vertex];
+
+    final_vertex = Matrix.multiply(matrices);
 
     return final_vertex;
 }
@@ -28,6 +45,35 @@ function CalculateTransformedVertex(vertex) {
 function ChangeTransform(index, type, values) {
     app.transforms[index].type = type;
     // update `app.transforms[index].mat4x4`
+    if(type.localeCompare("translate") == 0) {
+        var tx, ty, tz;
+        tx = values[0];
+        ty = values[1];
+        tz = values[2];
+        Mat4x4Translate(app.transforms[index].mat4x4, tx, ty, tz);
+    }
+    else if(type.localeCompare("scale") == 0) {
+        var sx, sy, sz;
+        sx = values[0];
+        sy = values[1];
+        sz = values[2];
+        Mat4x4Scale(app.transforms[index].mat4x4, sx, sy, sz);
+    }
+    else if(type.localeCompare("rotate_x") == 0) {
+        console.log(values[0]);
+        var theta = values[0] * Math.PI / 180.0;
+        console.log(theta);
+        console.log(-1 *Math.sin(theta));
+        Mat4x4RotateX(app.transforms[index].mat4x4, theta);
+    }
+    else if(type.localeCompare("rotate_y") == 0) {
+        var theta = values[0] * Math.PI / 180.0;
+        Mat4x4RotateY(app.transforms[index].mat4x4, theta);
+    }
+    else if(type.localeCompare("rotate_z") == 0) {
+        var theta = values[0] * Math.PI / 180.0;
+        Mat4x4RotateZ(app.transforms[index].mat4x4, theta);
+    }
 
     // recalculate compound transform and tranformed vertex
     app.compound = CalculateCompoundTransform(app.transforms);
